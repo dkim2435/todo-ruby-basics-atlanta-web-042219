6270dbbb2b9659b695de8ed571146e484a3a7689
Evan Kim <dkim2435@gmail.com>
2019-04-17 17:20:00 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
+  name=value
+end
+
+def argue(argument)
+  argument
   
 end
 
-def argue
-  
-end
-
-def greeting
-
+def greeting (hello, name)
+puts "#{hello}, #{name}"
 end
 
 def return_a_value
+  "hello"
 
 end
 
 def last_evaluated_value
+  "Here"
+  "there"
 
 end
 
-def pizza_party
+def pizza_party(pie="cheese")
+  pie
 
 end
